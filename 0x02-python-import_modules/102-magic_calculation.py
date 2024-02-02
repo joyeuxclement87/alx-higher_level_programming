3ad2d906d99bf78391e8b45d33b1ac2733b42971
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-02-02 07:11:46 -0800

    working on magic calc

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for j in range(4, 8):
+        return (add(c, j))
+    else:
+        res = sub(a, b)
+        return(res)
