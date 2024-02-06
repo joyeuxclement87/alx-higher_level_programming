b303b02d81431ed6f237e0373fdeaf626990ac0a
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-02-06 11:58:56 -0800

    working on square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""module for square"""
 
 
 class Square:
+    """Defining sqr"""
 
     def __init__(self, size):
+        """Cnstrctr.
+        Args:
+            size: size of the length
+        """
         self.__size = size
