589e311217ed2e9f2423a4fab60195c35c8fe57d
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-03-27 13:26:52 +0200

    working on my priveleges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- working on listing all privelages on server
-SELECT * FROM mysql.user WHERE USER IN ('user_0d_1','user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'loclhost';
