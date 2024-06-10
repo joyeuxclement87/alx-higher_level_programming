2d1050daddfe9baf82aaac8c52afec07c57915bc
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-06-10 11:05:06 +0100

    working on second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (n <= 3) {
   console.log(0);
 } else {
   const ar = process.argv.slice(2);
-  const arInt = ar.sort((a, b) => a - b);
+  const arInt = ar.sort((j, h) => j - h);
   console.log(arInt[arInt.length - 2]);
 }
