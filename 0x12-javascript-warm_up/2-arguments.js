faa361ef2c1c8da5e52ae7e6eccdcc3a8f649af8
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-06-10 10:15:46 +0100

    working on args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-function printArgMsg(...args) {
-    if (args.length === 0) {
-        console.log("No argument");
-    } else if (args.length === 1) {
-        console.log("Argument found");
-    } else {
-        console.log("Arguments found");
-    }
-}
-printArgMsg(...args);
+if (process.argv.length === 2) {
+    console.log('No argument');
+  } else if (process.argv.length > 3) {
+    console.log('Arguments found');
+  } else {
+    console.log('Argument found');
+  }
